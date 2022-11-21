5b8b84d4e35ee0f1f7edc1dff48abc323fedbf26
Humzas <humzas@umich.edu>
2022-11-21 12:46:37 -0500

    finalized code, readme, and comments

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # WeatherDashboard
 
 ## Description
+
+A weather dashboard the pull the weather of your city of choice.  It displays the current weather as well as the next five days.  It pulls from the Open Weather Map api. The website is also structured with responsive design as a priority, so it is mobile friendly.
+
+## Usage
+
+Type in the city you would like to search the weather for in the search input box.  The website will feed it into a coordinates function to return the geographical coordinates and feed that into a function to fetch your five day forecast.  The web page is responsively designed and is intended to be mobile friendly.
+
+## Demo
+
+Here is a screenshot of the deployed site ![image](./assets/images/demo.jpg)
+The live site is deployed [here](https://humzashaukat.github.io/WeatherDashboard/)
+
+## Credits 
+
+Icons were used and compiled by Erik Flowers on his deployed [site](http://erikflowers.github.io/weather-icons)
+
+## License
+
+An MIT License is used
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var futureWeather = document.querySelector("#fiveDay");
 var searchHistory = [];
 
 
-function getCountryCoordinates(search) {
+function getCountryCoordinates(search) { //inputs search query into geocoordinate api to return latitude and longitude of city
     var api_call = "//api.openweathermap.org/geo/1.0/direct?q=" + search + "&limit=1" + api_key;
     fetch (api_call)
     .then(function (response) {
@@ -15,13 +15,13 @@ function getCountryCoordinates(search) {
       })
       .then(function (data) {
         var lat = data[0].lat.toFixed(2);
-        var long = data[0].lon.toFixed(2);
+        var long = data[0].lon.toFixed(2); //latitude and longitude are trunkated to 2 decimals before being passed to weather api
         getWeather(lat,long);
         }
       );
 }
 
-function getWeather(lat,long) {
+function getWeather(lat,long) { //clears previous appended elements before calling weather api to dynamically create the new cards
   currentWeather.innerHTML = "";
   futureWeather.innerHTML = "";
     var weather_API = "//api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&units=imperial" + api_key;
@@ -33,8 +33,8 @@ function getWeather(lat,long) {
         printFiveDay();
         var count = 1;
         for (var i = 0; i < 40 ; i = i +7) {
-          if (i == 0) {
-            var cityName = $("<h2>").text(data.city.name + " " + dayjs().format("MM/DD/YYYY"));
+          if (i == 0) { //i = 0 will always be the current weather and time and a different container is used for that
+            var cityName = $("<h2>").text(data.city.name + " " + dayjs().format("MM/DD/YYYY")); 
             var icon = $("<i class = 'wi wi-owm-" + data.list[i].weather[0].id + "'></i>");
             var currentTemp = $("<h3>").text("Temperature: " + data.list[i].main.temp + " °F");
             var currentWind = $("<h3>").text("Wind: " + data.list[i].wind.speed + " MPH");
@@ -59,7 +59,7 @@ function getWeather(lat,long) {
       });
 } 
 
-function printFiveDay() {
+function printFiveDay() { //prints the next 5 days and assigns the div to class card and id with the day number
   for (var i = 1; i < 6; i++) {
     var dayIndex = dayjs().add(i,"day").format("MM/DD/YYYY");
     var dayBox = $("<div id = '" + i +"' class = '" + "card'>");
@@ -69,7 +69,7 @@ function printFiveDay() {
   }
 }
 
-function printSearchHistory(history) {
+function printSearchHistory(history) { //creates buttons for the search history. arbitrary value is last 5 searches
   searchHistoryList.innerHTML = "";
   for (var i = 0; i < history.length; i++) {
     var searchBtn = $("<button class = 'historyBtn'>");
@@ -79,7 +79,7 @@ function printSearchHistory(history) {
 }
 
 $(function() {
-  searchBtn.addEventListener("click", function(event) {
+  searchBtn.addEventListener("click", function(event) { //onclick of search button, the search is added to local history and the function to get coordinates and weather is called
     event.preventDefault();
     $("#futureWeather").attr("style", "display: flex");
     $("#currentWeather").attr("style", "display: flex");
@@ -94,24 +94,24 @@ $(function() {
     currentWeather.innerHTML = "";
     futureWeather.innerHTML = "";
     $("#search").val("");
-    getCountryCoordinates(search);
+    getCountryCoordinates(search);  //if the search history array is not filled, the new search will be pushed
     if (searchHistory.length < 5) {
       searchHistory.push(search);
       localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
       printSearchHistory(searchHistory);
-    } else {
+    } else { //if the search history array is filled, the first element is shifted and new search is pushed
       searchHistory.shift();
       searchHistory.push(search);
       localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
       printSearchHistory(searchHistory);
     }
   });
-  $("#searchHistory").click(function(event) {
+  $("#searchHistory").click(function(event) { //if the search history button is clicked, that weather for that city will be retrieved 
     event.preventDefault();
     var historyEntry = event.target;
     if (historyEntry.matches("button") == true ) {
-    search = historyEntry.textContent;
-    getCountryCoordinates(search);
+      var search = historyEntry.textContent;
+      getCountryCoordinates(search);
     }
   })
 })
